6a780fe9edf5fe04bc0d3515e8caf2c22b479854
314lipczuk <3.14lipczuk@gmail.com>
2024-04-12 14:23:09 +0200

    distance function, fix bug, more testing

--- needleman-wunsch-algorithm/main.py
+++ needleman-wunsch-algorithm/main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 import sys
 
-# current version only works with simple gap costs - G = -2 is the same option as querying BLAST with gap costs {existence:0, extension:2} - i might add support for more complex types in the future
+# current version only works with simple gap costs f.ex G = -2 is the same option as querying BLAST with gap costs {existence:0, extension:2} - i might add support for more complex types in the future
 
 S = [
 	# A  G   C   T
@@ -32,35 +32,36 @@ class NWConfig:
   def lookup(self, a, b):
     return self.matrix[base_idx[a]][base_idx[b]]
 
+  def fill(self, m, seq1, seq2, aggr_fun=max):
+    for j in range(len(seq2)):
+      for i in range(len(seq1)):
+        self.fill_single(m, [j+1, i+1], seq1, seq2, aggr_fun=aggr_fun)
+
+  def fill_single(self, m, pos, seq1, seq2, aggr_fun):
+    j,i = pos
+    assert i > 0 and j > 0
+    l_j, l_i = [seq2[j-1], seq1[i-1]]
+    score = aggr_fun([
+      [(m[j-1][i-1][0] + self.lookup(l_j, l_i)), Direction.DIAG],
+      [(m[j-1][i][0] + self.penalty), Direction.UP],
+      [(m[j][i-1][0] + self.penalty), Direction.LEFT],
+      ], key=lambda x: x[0])
+    m[j][i] = score
+
+
 class Direction(Enum):
   DONE = 0
   DIAG = 1
   UP = 2
   LEFT = 3
 
-def make_initial_matrix(seq1, seq2):
+def make_initial_matrix(seq1, seq2, config):
   m = [[[0,None] for _ in range(len(seq1)+1)] for _ in range(len(seq2)+1)]
-  for j in range(len(seq1)+1): m[0][j] = [G * j, Direction.LEFT]
-  for i in range(len(seq2)+1): m[i][0] = [G * i, Direction.UP]
+  for j in range(len(seq1)+1): m[0][j] = [config.penalty * j, Direction.LEFT]
+  for i in range(len(seq2)+1): m[i][0] = [config.penalty * i, Direction.UP]
   m[0][0][1] = Direction.DONE
   return m
 
-
-def fill_single(m, pos, seq1, seq2):
-  j,i = pos
-  assert i > 0 and j > 0
-  l_j, l_i = [seq2[j-1], seq1[i-1]]
-  score = max([
-    [(m[j-1][i-1][0] + config.lookup(l_j, l_i)), Direction.DIAG],
-    [(m[j-1][i][0] + G), Direction.UP],
-    [(m[j][i-1][0] + G), Direction.LEFT],
-    ], key=lambda x: x[0])
-  m[j][i] = score
-
-def fill(m, seq1, seq2):
-  for j in range(len(seq2)):
-    for i in range(len(seq1)):
-      fill_single(m, [j+1, i+1], seq1, seq2)
 
 
 def solve(m, seq1, seq2):
@@ -107,13 +108,27 @@ def test():
     seq2 = read_fasta(f.read())
   with open('./../static/test/needleman_wunsch/mouse_HoxA13.fa','r') as f:
     seq1 = read_fasta(f.read())
-  R = make_initial_matrix(seq1, seq2)
-  global config
-  config = NWConfig().from_matrix(S).with_penalty(R)
-  fill(R, seq1, seq2)
+  config = NWConfig().from_matrix(S).with_penalty(G)
+  R = make_initial_matrix(seq1, seq2, config)
+  config.fill(R, seq1, seq2)
   _,_,score = solve(R, seq1, seq2)
   assert score == 840
-  print("test passed")
+  print("test 1 - alignment score for HoxA13 in human and mouse the same as from blast\npassed")
+
+  R = make_initial_matrix(seq1, seq2, config)
+  config = NWConfig().from_rules({'match':1, 'mismatch':-2}).with_penalty(G)
+  config.fill(R, seq1, seq2)
+  _,_,score2 = solve(R, seq1, seq2)
+  assert score == score2
+  print("test 2 - score computed using rule-based type of config matches the original\npassed")
+
+  R = make_initial_matrix(seq1, seq1, config)
+  config = NWConfig().from_rules({'match':0, 'mismatch':1}).with_penalty(-G)
+  config.fill(R, seq1, seq1, aggr_fun=min)
+  _,_,score3 = solve(R, seq1, seq1)
+  print(score3)
+  assert score3 == 0
+  print("test 3 - score distance metric achieved by changing matrix and aggregation function\npassed")
 
 def main():
   if len(sys.argv) == 2 and sys.argv[-1] == 'test':
@@ -127,11 +142,15 @@ def main():
     seq2 = read_fasta(f.read())
   with open(sys.argv[2],'r') as f:
     seq1 = read_fasta(f.read())
-  R = make_initial_matrix(seq1, seq2)
-  global config
-  config = NWConfig().from_matrix(S).with_penalty(R)
-  fill(R, seq1, seq2)
+  config = NWConfig().from_matrix(S).with_penalty(G)
+  R = make_initial_matrix(seq1, seq2, config)
+  config.fill(R, seq1, seq2)
   print(solve(R, seq1, seq2))
   
 if __name__ == "__main__":
   main()
+
+# how do you make a distance metric?
+# well, in theory i could do it by substituting the aggregating function to min and the S matrix into diagonal zeros and all the rest positive ints
+# what about gaps? change into positive numbers i guess
+# aight, let's try that
